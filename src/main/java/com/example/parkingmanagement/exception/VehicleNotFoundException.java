b749b42e2cf98e428e2ae1955a1ff6560614f09d
NandaniRajput876 <nandanirajput876@gmail.com>
2026-08-17 16:42:34 +0530

    Initial commit1

---
+++ src/main/java/com/example/parkingmanagement/exception/VehicleNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.parkingmanagement.exception;
+
+public class VehicleNotFoundException extends RuntimeException{
+    public VehicleNotFoundException (String message){
+        super(message);
+
+    }
+}
+
